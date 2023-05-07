45b3baa9b36ae58d4cd4400efdda58a066dcb63e
Ankush Madankar <ankush.madankar@siemens.com>
2023-05-07 13:27:09 +0530

    Updated xml comments for interfaces

--- ArithmeticStrategyOperation/Interfaces/IParsingStrategy.cs
+++ ArithmeticStrategyOperation/Interfaces/IParsingStrategy.cs
@@ -1,7 +1,14 @@
 ﻿namespace ArithmeticStrategyOperation.Interfaces
 {
+    /// <summary>
+    /// Define the parsing strategy how values will be operated.
+    /// </summary>
     public interface IParsingStrategy
     {
+        /// <summary>
+        /// Parse numbers with required strategy e.g. Sum, Multiplication
+        /// </summary>
+        /// <returns></returns>
         Task<double> ParseNumbers();
     }
 }
--- ArithmeticStrategyOperation/Interfaces/IWriteOutputStrategy.cs
+++ ArithmeticStrategyOperation/Interfaces/IWriteOutputStrategy.cs
@@ -1,7 +1,16 @@
 ﻿namespace ArithmeticStrategyOperation.Interfaces
 {
+    /// <summary>
+    /// Write output Strategy. Output could be anything like Console, File or Message Queue. 
+    /// </summary>
     public interface IWriteOutputStrategy
     {
+        /// <summary>
+        /// Write output to different source like Console, File, Message Queue or Email
+        /// </summary>
+        /// <typeparam name="TResult">Type of output</typeparam>
+        /// <param name="result"></param>
+        /// <returns></returns>
         Task Write<TResult>(TResult result);
     }
 }
